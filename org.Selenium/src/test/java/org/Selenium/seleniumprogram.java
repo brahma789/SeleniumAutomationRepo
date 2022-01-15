47b94131eeb58453306d8781ddc9fa7a33436ad2
brahma789 <brahma789@gmail.com>
2022-01-15 16:18:42 +0530

    Selenium

---
+++ org.Selenium/src/test/java/org/Selenium/seleniumprogram.java
@@ -0,0 +1,24 @@
+package org.Selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class seleniumprogram {
+
+	public static void main(String[] args) throws Exception {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://opsintuat-org-uat-mta-cf-cockpit-approuter.internal.cfapps.sap.hana.ondemand.com/sapcpidevopscfcockpit/index.html");
+		driver.manage().window().maximize();
+		driver.findElementByClassName("saml-login-link").click();
+		driver.findElementById("j_otpcode");
+		Thread.sleep(6000);
+	//	driver.findElementByClassName("ids-button ids-button--primary").click();
+		 driver.findElementById("logon_continue").click();
+		
+
+	}
+
+}
