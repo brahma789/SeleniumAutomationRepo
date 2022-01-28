53f71c66899f86b952ebe3959e686b52204f5e38
brahma789 <brahma789@gmail.com>
2022-01-28 18:24:13 +0530

    Updated

---
+++ Selenium/src/Automation/SeleniumProg1.java
@@ -0,0 +1,12 @@
+package Automation;
+
+public class SeleniumProg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Selenium Prog First Class");
+		System.out.println("Selenium Prog First Class12");
+
+	}
+
+}
